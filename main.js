9f8c34952f030fba70f92b75f4f0726910aed625
Dinoosawruss <m.dormand@sky.com>
2018-10-21 16:06:38 +0100

    Added some portfolio things

--- main.js
+++ main.js
@@ -42,3 +42,23 @@ function setActiveC(button) {
       document.getElementById("main").classList.toggle("home");
 
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+    document.getElementById("start").id.remove("start");
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
