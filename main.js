0cead231185c9bdf82e1fa9f13f8f9e6102d5d34
Dinoosawruss <m.dormand@sky.com>
2018-10-21 13:30:02 +0100

    Added page switching system and did other tidying

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
-function setActive(button) {
+function setActiveH(button) {
     document.getElementsByClassName("active")[0].className = "";
 
     button.className = "active";
+
+    document.getElementById("main").classList.add("home");
+
+    if(document.getElementById("main").classList.contains("portfolio") )
+      document.getElementById("main").classList.toggle("portfolio");
+
+    else if (document.getElementById("main").classList.contains("contact") )
+      document.getElementById("main").classList.toggle("contact");
+
+}
+
+function setActiveP(button) {
+    document.getElementsByClassName("active")[0].className = "";
+
+    button.className = "active";
+
+    document.getElementById("main").classList.add("portfolio");
+
+    if(document.getElementById("main").classList.contains("home") )
+      document.getElementById("main").classList.toggle("home");
+
+    else if (document.getElementById("main").classList.contains("contact") )
+      document.getElementById("main").classList.toggle("contact");
+
+}
+
+function setActiveC(button) {
+    document.getElementsByClassName("active")[0].className = "";
+
+    button.className = "active";
+
+    document.getElementById("main").classList.add("contact");
+
+    if(document.getElementById("main").classList.contains("portfolio") )
+      document.getElementById("main").classList.toggle("portfolio");
+
+    else if (document.getElementById("main").classList.contains("home") )
+      document.getElementById("main").classList.toggle("home");
+
 }
